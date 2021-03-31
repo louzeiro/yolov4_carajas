1e81200c2cf0de61381ed16cfbd0e995670b917b
Flaviane Louzeiro <flalouzeiro@hotmail.com>
2021-03-31 09:56:26 -0300

    Update funcoes.py

--- yolo/util/funcoes.py
+++ yolo/util/funcoes.py
@@ -20,3 +20,14 @@ def detectar(imagem):
   os.system("./darknet detector test data/obj.data cfg/yolov4_drone.cfg yolov4_drone_best.weights {}".format(caminhoImagem))
   exibir("predictions.jpg")
   os.rename("predictions.jpg", 'results/resultado_{}'.format(imagem)) #salvando a predição na pasta results
+  
+def arquivosConf():
+## Copiando os arquivos de configuração
+  os.system('cp ../yolo/confs/obj.names data/')
+  os.system('cp ../yolo/confs/obj.data data/')
+  os.system('cp ../yolo/confs/yolov4_drone.cfg cfg/')
+  
+  print('Carregando os pesos para o ambiente'...)
+  os.system('wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=17K3pgrjxjkUdCJfwpbTMTMQx8ObEzA9q" -O- | sed -rn "s/.*confirm=([0-9A-Za-z_]+).*/\1\n/p")&id=17K3pgrjxjkUdCJfwpbTMTMQx8ObEzA9q" -O yolov4_drone_best.weights && rm -rf /tmp/cookies.txt')
+  print('Pesos carregados')
+
